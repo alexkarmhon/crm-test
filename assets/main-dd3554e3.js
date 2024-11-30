84cd8fa731bad6707d268b346641b1bad8ac725b
alexkarmhon <132042184+alexkarmhon@users.noreply.github.com>
2024-11-30 14:37:37 +0000

    Deploying to gh-pages from @ alexkarmhon/crm-test@d7476533f5dbe5909741a7896673d392a1ff34c0 🚀

--- assets/main-dd3554e3.js
+++ assets/main-dd3554e3.js
@@ -40,4 +40,28 @@
   }
 })();
 const style = "";
+document.querySelector("aside");
+document.querySelector("main");
+document.querySelector(".body-wrapper");
+const adjustMainMargin = () => {
+  const aside = document.querySelector("aside");
+  const main = document.querySelector("main");
+  const wrapper = document.querySelector(".body-wrapper");
+  const isWrapperBlock = getComputedStyle(wrapper).flexDirection === "column";
+  if (aside && main && isWrapperBlock) {
+    main.style.marginTop = `${aside.offsetHeight}px`;
+    main.style.marginLeft = "0";
+  } else {
+    main.style.marginTop = "0";
+    main.style.marginLeft = `${aside.offsetWidth}px`;
+  }
+};
+window.addEventListener("load", adjustMainMargin);
+window.addEventListener("resize", adjustMainMargin);
+const currentLink = document.querySelector(".current");
+const currentPageTitle = document.querySelector(".page-title");
+const setCurrentPageTitle = () => {
+  currentPageTitle.textContent = currentLink.textContent;
+};
+window.addEventListener("DOMContentLoaded", setCurrentPageTitle);
 console.log("hello crm");
